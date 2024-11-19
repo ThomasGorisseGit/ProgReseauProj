f1eec2fa8fcdcf81ac536a99ff743bb12dcfe6f4
Isaline <isaline.foissey@gmail.com>
2024-11-19 16:59:13 +0100

    génération aléatoire fonctionnelle

--- Makefile
+++ Makefile
@@ -1,4 +1,3 @@
-```
 CC = gcc
 CFLAGS = 
 BIN_DIR = bin
@@ -8,19 +7,14 @@ all: $(BIN_DIR)/server $(BIN_DIR)/client
 
 # Compilation du serveur
 $(BIN_DIR)/server: serveur/controller/controller.c serveur/model/donnees/awale.c serveur/model/services/lobbyManager.c serveur/model/services/messageManager.c serveur/model/services/partieManager.c serveur/vue/echange.c serveur/controller/commandes.c
-    @mkdir -p $(BIN_DIR)
-    $(CC) $(CFLAGS) -o $(BIN_DIR)/server serveur/controller/controller.c serveur/model/donnees/awale.c serveur/model/services/lobbyManager.c serveur/model/services/messageManager.c serveur/model/services/partieManager.c serveur/vue/echange.c serveur/controller/commandes.c
+	@mkdir -p $(BIN_DIR)
+	$(CC) $(CFLAGS) -o $(BIN_DIR)/server serveur/controller/controller.c serveur/model/donnees/awale.c serveur/model/services/lobbyManager.c serveur/model/services/messageManager.c serveur/model/services/partieManager.c serveur/vue/echange.c serveur/controller/commandes.c
 
 # Compilation du client
 $(BIN_DIR)/client: client/controller/controller.c client/model/messageManager.c client/vue/display.c
-<<<<<<< HEAD
-    @mkdir -p $(BIN_DIR)
-    $(CC) $(CFLAGS) -o $(BIN_DIR)/client client/controller/controller.c client/model/messageManager.c client/vue/display.c
-=======
 	@mkdir -p $(BIN_DIR)
 	$(CC) $(CFLAGS) -o $(BIN_DIR)/client client/controller/controller.c client/model/messageManager.c client/vue/display.c
->>>>>>> origin/merge
 
 # Nettoyage des fichiers compilés
 clean:
-    rm -rf $(BIN_DIR)
+	rm -rf $(BIN_DIR)
--- serveur/model/services/partieManager.c
+++ serveur/model/services/partieManager.c
@@ -59,6 +59,10 @@ void gerer_coup(Jeu *jeu, Joueur *joueur, int case_depart)
 void qui_commence(Jeu *jeu)
 {
     int nombreAleatoire = (rand() % 2) + 1;
+    printf("\033[31m"
+           "Le nombre généré est %d\n", // Remplacement par %d pour un entier
+           nombreAleatoire);
+
     if (nombreAleatoire == 1)
     {
         jeu->current = jeu->joueur1;
@@ -68,6 +72,7 @@ void qui_commence(Jeu *jeu)
         jeu->current = jeu->joueur2;
     }
 }
+
 void initialiser_jeu(Lobby *lobby, Joueur *demandeur, Joueur *joueur)
 {
     demandeur->status = PARTIE;
@@ -75,30 +80,36 @@ void initialiser_jeu(Lobby *lobby, Joueur *demandeur, Joueur *joueur)
 
     // Initialiser la partie
     Jeu *jeu = malloc(sizeof(Jeu));
-    // vérification de l'existance du jeu
     if (jeu == NULL)
     {
         perror("Erreur d'allocation mémoire pour le jeu");
-        printf("Erreur d'allocation mémoire pour le jeu");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
+
     jeu->joueur1 = demandeur;
     jeu->joueur2 = joueur;
-
-    printf("Joueurs : %s et %s\n", jeu->joueur1->nom, jeu->joueur2->nom);
 
     if (jeu->joueur1 == NULL || jeu->joueur2 == NULL)
     {
         fprintf(stderr, "Les joueurs ne sont pas correctement initialisés.\n");
-        free(jeu); // Libération de la mémoire pour éviter une fuite
-        exit(1);
+        free(jeu);
+        exit(EXIT_FAILURE);
     }
+
     initialiserPlateau(jeu);
+
+    if (lobby->nbJeux >= MAX_PARTIES)
+    {
+        fprintf(stderr, "Le lobby est plein, impossible de démarrer un nouveau jeu.\n");
+        free(jeu);
+        exit(EXIT_FAILURE);
+    }
+
     joueur->idPartie = lobby->nbJeux;
     demandeur->idPartie = lobby->nbJeux;
 
     lobby->jeux[lobby->nbJeux] = jeu;
     lobby->nbJeux++;
-
+    srand(time(NULL));
     qui_commence(jeu);
 }
